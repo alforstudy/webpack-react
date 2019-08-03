e41686ced5bc0b41d5bd1a6214d2b512eb682cb2
安磊 <alforstudy@163.com>
2019-08-03 14:27:21 +0800

    装饰器

--- src/index.js
+++ src/index.js
@@ -1,36 +1,63 @@
-import React, { useState, useEffect, } from 'react';
-import ReactDOM from 'react-dom';
-import BindEvent from '@/components/bindEvent.jsx';
+// import React, { useState, useEffect, } from 'react';
+// import ReactDOM from 'react-dom';
 
 
-function Counter() {
-    //定义state
-    const [count,setCount,] = useState(0);
-    const [fruit, setFruit,] = useState('banana');
-    const [todos, setTodos,] = useState([{ text: 'Learn Hooks', },]);
-    //页面重新渲染时触发
-    useEffect(()=>{
-        document.title = count;
-    });
-    useEffect(() => {
-        document.title = `You clicked ${count} times`;
-    },[count]); // 仅在 count 更改时更新
-    return <div>
-        <p>fruit:{fruit}</p>
-        <div>
-            {
-                todos.map((item, index) => {
-                    return <p key={index}>{item.text}</p>;
-                })
-            }
-        </div>
-        <p>Count:{count}</p>
-        <button onClick={()=>setCount(count+1)} >Count+1</button>
-    </div>;
+// ReactDOM.render(<div>
+//     <Counter/>
+// </div>,document.getElementById('app'));
+
+function log(target){
+    debugger;
+    const desc = Object.getOwnPropertyDescriptors(target.prototype);
+    for(const key of Object.keys(desc)){
+        if(key === 'constructor'){
+            continue;
+        }
+        const func = desc[key].value;
+
+        if('function' === typeof func){
+            Object.defineProperty(target.prototype,key,{
+                value(...args) {
+                    console.log('before'+key);
+                    const ret = func.apply(this,args);
+                    console.log('after'+key);
+                    return ret;
+                }
+            });
+        }
+    }
 }
 
 
-ReactDOM.render(<div>
-    <Counter/>
-</div>,document.getElementById('app'));
+function readonly(target,key,descriptor){
+    descriptor.writable = false;
+}
 
+function validate(target,key,descriptor){
+    debugger;
+    const func = descriptor.value;
+    descriptor.value = function(...args){
+        for(const num of args){
+            if('number' !== typeof num){
+                throw new Error(`${num} is not a number`);
+            }
+            return func.apply(this,...args);
+        }
+    };
+}
+
+@log
+class Person{
+
+    @readonly PI = 3.1415926;
+
+    @validate
+    say(num){
+        console.log('Hello');
+    }
+}
+
+
+const p1 = new Person();
+p1.say('12');
+p1.PI = 12;
