3a8c24670653eabde8ebf7542639fd4ad0fcefe6
安磊 <alforstudy@163.com>
2019-08-05 09:41:50 +0800

    react-mobx

--- src/index.js
+++ src/index.js
@@ -1,63 +1,60 @@
-// import React, { useState, useEffect, } from 'react';
-// import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {observable,computed} from 'mobx';
+import {observer} from 'mobx-react';
 
 
-// ReactDOM.render(<div>
-//     <Counter/>
-// </div>,document.getElementById('app'));
-
-function log(target){
-    debugger;
-    const desc = Object.getOwnPropertyDescriptors(target.prototype);
-    for(const key of Object.keys(desc)){
-        if(key === 'constructor'){
-            continue;
-        }
-        const func = desc[key].value;
-
-        if('function' === typeof func){
-            Object.defineProperty(target.prototype,key,{
-                value(...args) {
-                    console.log('before'+key);
-                    const ret = func.apply(this,args);
-                    console.log('after'+key);
-                    return ret;
-                }
-            });
-        }
+class TodoList {
+    @observable todos = [];
+    @computed get unfinishedTodoCount() {
+        return this.todos.filter(todo => !todo.finished).length;
     }
 }
 
 
-function readonly(target,key,descriptor){
-    descriptor.writable = false;
-}
-
-function validate(target,key,descriptor){
-    debugger;
-    const func = descriptor.value;
-    descriptor.value = function(...args){
-        for(const num of args){
-            if('number' !== typeof num){
-                throw new Error(`${num} is not a number`);
-            }
-            return func.apply(this,...args);
-        }
-    };
-}
-
-@log
-class Person{
-
-    @readonly PI = 3.1415926;
-
-    @validate
-    say(num){
-        console.log('Hello');
+@observer
+class TodoListView extends Component {
+    render() {
+        return <div>
+            <ul>
+                {this.props.todoList.todos.map(todo =>
+                    <TodoView key={todo.id}
+                        todo={todo}
+                    />
+                )}
+            </ul>
+            Tasks left: {this.props.todoList.unfinishedTodoCount}
+        </div>;
     }
 }
 
+const TodoView = observer(({todo}) =>
+    <li>
+        <input
+            checked={todo.finished}
+            onClick={() => todo.finished = !todo.finished}
+            type="checkbox"
+        />{todo.title}
+    </li>
+);
 
-const p1 = new Person();
-p1.say('12');
-p1.PI = 12;
+
+
+const store = new TodoList();
+store.todos.push({
+    id:1,
+    finished:true
+},{
+    id:2,
+    finished:false
+},{
+    id:3,
+    finished:true
+});
+setTimeout(()=>{
+    store.todos.push({
+        id:4,
+        finished:false
+    });
+},3000);
+ReactDOM.render(<TodoListView todoList={store} />, document.getElementById('app'));
